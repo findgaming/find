0d8e1bd29ffe78056edac4c60c830ffb0b25c7ab
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-14 17:25:14 -0700

    Adding usersRouter to server route

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-
+const path = require('path');
 const PORT = 3000;
 
-app.use(express.json());
+const app = express();
+// app.use(express.json());
+
+const usersRouter = require('./routes/users');
 
 // for cross origin, making it needed for cases w/out webpack or proxy
 app.use(cors());
 
-app.get('/test', (req, res) => {
-  console.log('entered /test');
-  res.send({ testresponse: 'hit test endpoint' });
-});
+// router for users
+app.get('/api/users', usersRouter);
+
+// // add a user to the table
+// app.get('/test', (req, res) => {
+//   console.log('entered /test');
+//   res.send({ testresponse: 'hit test endpoint' });
+// });
 
 app.listen(PORT, () => {
   console.log('server is listening on port: ', PORT);
