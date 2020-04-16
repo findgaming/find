ff09be1c63b52b370e884cb3c90cf58219ea9dc2
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-16 14:20:00 -0700

    Chatroom link apears in the chat room window after timer has expired

--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -1,3 +1,4 @@
+import CountDown from 'react-native-countdown-component';
 import React, { useState, useEffect } from 'react';
 import {
   Image,
@@ -15,15 +16,58 @@ import io from 'socket.io-client';
 
 import { MonoText } from '../components/StyledText';
 
+// const styles = StyleSheet.create({
+//   playButton: {
+//     alignSelf: 'center',
+//     flexWrap: 'nowrap',
+//     backgroundColor: '#3498DB',
+//     borderBottomColor: '#2980B9',
+//     borderBottomWidth: '5px',
+//     textAlign: 'center',
+//     borderRadius: '15px',
+//     paddingHorizontal: '20px',
+//     zIndex: '99',
+//     color: '#fff'
+//   }
+// });
+
 const socket = io('http://localhost:3000');
+
+const PlayLink = () => (
+  <center>
+    <a
+      style={{
+        textDecoration: 'none',
+        backgroundColor: '#3498DB',
+        paddingTop: '10px',
+        paddingRight: '8px',
+        paddingLeft: '8px',
+        paddingBottom: '10px',
+        borderBottomWidth: '5px',
+        borderBottomColor: '#2980B9',
+        borderRadius: '8px',
+        boxShadow: '5px 7px 3px 0px rgba(0,0,0,0.75)',
+        color: '#fff',
+        fontWeight: '900',
+        marginTop: '10px',
+        marginBottom: '10px',
+        position: 'relative'
+      }}
+      href="https://jackbox.tv/"
+    >
+      PLAY NOW
+    </a>
+  </center>
+);
 
 const ChatRoomScreen = ({ route, navigation }) => {
   const { title } = route.params;
   const [myMessage, setMyMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [play, setPlay] = useState(false);
   let chatMessages;
 
-  // useEffect(() => {}, []);
+  // create a return link to set to true
 
   socket.on('chat message', msg => {
     setMessages(messages.concat(msg));
@@ -39,8 +83,29 @@ const ChatRoomScreen = ({ route, navigation }) => {
     setMyMessage('');
   }
 
+  // const releaseGameLink = () => {
+  //   // alert('WORKED');
+  //   // alert('WORKED2');
+  //   let returnLink = 'booogie';
+  //   console.log('return element');
+  //   return returnLink;
+  // };
+
+  // returnLink ? console.log('this is returnlink', returnLink) : null;
+  console.log('this is play', play);
   return (
     <View>
+      <CountDown
+        until={1}
+        onFinish={() => {
+          // releaseGameLink();
+          setPlay(true);
+        }}
+        onPress={() => alert('hello')}
+        size={30}
+      />
+      {play ? <PlayLink></PlayLink> : null}
+
       <TextInput
         style={{ height: 40, borderWidth: 2, top: 600 }}
         autoCorrect={false}
