dbcbc142dd9931c85981de4aeb8158a54fc11489
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-16 18:08:16 -0700

    improted Clipbard from react native

--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
   TextInput,
   View,
-  Button
+  Button,
+  Clipboard
 } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -52,11 +53,11 @@ const ChatRoomScreen = ({ route, navigation }) => {
   const [play, setPlay] = useState(false);
   let chatMessages;
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', msg => {
     setMessages(messages.concat(msg));
   });
 
-  chatMessages = messages.map((chatMessage) => {
+  chatMessages = messages.map(chatMessage => {
     const messageStyle =
       chatMessage.username === username
         ? styles.userMessage
@@ -79,44 +80,43 @@ const ChatRoomScreen = ({ route, navigation }) => {
     setMyMessage('');
   }
 
-  const renderPlayArea = () => {
-    play ? (
-      <View style={styles.playButtonWrapper}>
-        <View>
-          <PlayLink></PlayLink>
-        </View>
-        <TouchableOpacity onPress={() => Clipboard.setString('SCFB')}>
-          <View style={styles.textView}>
-            <Text
-              style={{
-                color: '#008000',
-                fontSize: 14,
-                backgroundColor: '#00FF7F',
-                fontFamily: 'Arial',
-                fontWeight: 900,
-                fontStyle: 'bold',
-                textAlign: 'center',
-                paddingTop: 10,
-                paddingRight: 8,
-                paddingLeft: 8,
-                paddingBottom: 10,
-                borderBottomWidth: 5,
-                borderBottomColor: '#008000',
-                boxShadow: '5px 7px 3px 0px rgba(0,0,0,0.75)',
-                marginTop: 10,
-                marginBottom: 10,
-                width: 120,
-                borderRadius: 8
-              }}
-            >
-              SCFB
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    ) : null;
-  };
-
+  // const renderPlayArea = () => {
+  //   play ? (
+  //     <View style={styles.playButtonWrapper}>
+  //       <View>
+  //         <PlayLink></PlayLink>
+  //       </View>
+  //       <TouchableOpacity onPress={() => Clipboard.setString('SCFB')}>
+  //         <View style={styles.textView}>
+  //           <Text
+  //             style={{
+  //               color: '#008000',
+  //               fontSize: 14,
+  //               backgroundColor: '#00FF7F',
+  //               fontFamily: 'Arial',
+  //               fontWeight: 900,
+  //               fontStyle: 'bold',
+  //               textAlign: 'center',
+  //               paddingTop: 10,
+  //               paddingRight: 8,
+  //               paddingLeft: 8,
+  //               paddingBottom: 10,
+  //               borderBottomWidth: 5,
+  //               borderBottomColor: '#008000',
+  //               boxShadow: '5px 7px 3px 0px rgba(0,0,0,0.75)',
+  //               marginTop: 10,
+  //               marginBottom: 10,
+  //               width: 120,
+  //               borderRadius: 8
+  //             }}
+  //           >
+  //             SCFB
+  //           </Text>
+  //         </View>
+  //       </TouchableOpacity>
+  //     </View>
+  //   ) : null;
+  // };
   return (
     <View>
       <CountDown
@@ -128,7 +128,41 @@ const ChatRoomScreen = ({ route, navigation }) => {
         onPress={() => alert('hello')}
         size={30}
       />
-      {renderPlayArea()}
+      {play ? (
+        <View style={styles.playButtonWrapper}>
+          <View>
+            <PlayLink></PlayLink>
+          </View>
+          <TouchableOpacity onPress={() => Clipboard.setString('SCFB')}>
+            <View style={styles.textView}>
+              <Text
+                style={{
+                  color: '#008000',
+                  fontSize: 14,
+                  backgroundColor: '#00FF7F',
+                  fontFamily: 'Arial',
+                  fontWeight: 900,
+                  fontStyle: 'bold',
+                  textAlign: 'center',
+                  paddingTop: 10,
+                  paddingRight: 8,
+                  paddingLeft: 8,
+                  paddingBottom: 10,
+                  borderBottomWidth: 5,
+                  borderBottomColor: '#008000',
+                  boxShadow: '5px 7px 3px 0px rgba(0,0,0,0.75)',
+                  marginTop: 10,
+                  marginBottom: 10,
+                  width: 120,
+                  borderRadius: 8
+                }}
+              >
+                SCFB
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      ) : null}
       <View style={styles.inputWrapper}>
         <TextInput
           style={styles.input}
@@ -136,7 +170,7 @@ const ChatRoomScreen = ({ route, navigation }) => {
           autoCorrect={false}
           value={myMessage}
           onSubmitEditing={submitChatMessage}
-          onChangeText={(value) => setMyMessage(value)}
+          onChangeText={value => setMyMessage(value)}
         />
         <TouchableOpacity style={styles.button}>
           <Text
